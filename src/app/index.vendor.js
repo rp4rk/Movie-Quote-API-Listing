1fe5c634eb917028c7a85ac486663f1393015498
Ryan Park <Parky.fp@gmail.com>
2016-05-12 15:58:00 +0100

    Boilerplate

---
+++ src/app/index.vendor.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// node_modules
+import "angular";
+
+import "angular-ui-router";
+
+
+import 'jquery';
+
+
+
+import "angular-resource";
+
+import "angular-resource";
+
+
+
+// local scripts
+//import "../assets/js/...";
